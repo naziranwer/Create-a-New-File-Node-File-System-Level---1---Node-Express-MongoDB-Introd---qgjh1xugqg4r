242b4e80b9e195980649a2207b9f18327c628b73
naziranwer <122814116+naziranwer@users.noreply.github.com>
2023-08-04 22:58:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-// const fs = require("fs/promises");
-// const fileName = "myfile.txt";
-// const fileContent = "Newton School";
-// const writeFile = async (fileName, fileContent) => {
-//   // write code here
-//   // dont change function name
-// };
+const fs = require("fs/promises");
+const fileName = "myfile.txt";
+const fileContent = "Newton School";
+const writeFile = async (fileName, fileContent) => {
+  // write code here
+  // dont change function name
+  fs.writeFile(fileName,fileContent, (err)=>{
+    if(err){
+      console.log(err);
+  }});
+};
 
 // module.exports = { writeFile };
 
-const fs = require('node:fs');
+// const fs = require('node:fs');
 
-const writeFile= async (filename, content)=> {
-  // Create a new file with the given filename and content
-  fs.writeFile(filename, content, (err) => {
-    if (err) {
-      // If there's an error, log the error message
-      console.error(err);
+// const writeFile= async (filename, content)=> {
+//   // Create a new file with the given filename and content
+//   fs.writeFile(filename, content, (err) => {
+//     if (err) {
+//       // If there's an error, log the error message
+//       console.error(err);
 
-  }});
-}
+//   }});
+// }
 
-// Create a new file with the name 'myfile.txt' and the content 'Newton School'
-writeFile('myfile.txt', 'Newton School');
+// // Create a new file with the name 'myfile.txt' and the content 'Newton School'
+// writeFile('myfile.txt', 'Newton School');
 
 
